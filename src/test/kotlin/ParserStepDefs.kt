e84d7ac3510f7d37fe567624f5e25eb52372c968
Graeme Lockley <graemel@no9.co.za>
2018-06-10 14:56:55 +0200

    refactor: remove State object by floating the fields into ParserStepDefs

--- src/test/kotlin/ParserStepDefs.kt
+++ src/test/kotlin/ParserStepDefs.kt
@@ -4,37 +4,34 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 
-private object State {
-    var inputText: String? = null
-
-    var parseResult: ParseResult = value("")
-}
-
-
 class ParserStepDefs : En {
     init {
+        var inputText: String? = null
+
+        var parseResult: ParseResult = value("")
+
         Given("the sle text {string}") { string: String ->
-            State.inputText = string
+            inputText = string
         }
 
         When("I parse the supplied input as a module") {
-            val inputText = State.inputText
+            val text = inputText
 
-            if (inputText == null) {
+            if (text == null) {
                 throw IllegalArgumentException("Unable to parse as no input has been supplied")
             } else {
-                State.parseResult = parseText(inputText)
+                parseResult = parseText(text)
             }
         }
 
         Then("the parse tree is {string}") { string: String ->
-            val parseTree = State.parseResult.right()
+            val parseTree = parseResult.right()
 
             assertEquals(string, parseTree ?: "")
         }
 
         Then("I expect a syntax error to be reported") {
-            assertNotNull(State.parseResult.left())
+            assertNotNull(parseResult.left())
         }
     }
 }
