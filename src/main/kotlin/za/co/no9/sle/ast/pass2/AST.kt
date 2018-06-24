4ff77072abc4f65beecd896d1c7b88c0c31b25fe
Graeme Lockley <graemel@no9.co.za>
2018-06-24 08:43:16 +0200

    feature: create pass2 AST

---
+++ src/main/kotlin/za/co/no9/sle/ast/pass2/AST.kt
@@ -0,0 +1,62 @@
+package za.co.no9.sle.ast.pass2
+
+import za.co.no9.sle.ast.Location
+
+
+sealed class Node(
+        open val location: Location)
+
+
+data class Module(
+        override val location: Location,
+        val declarations: List<Declaration>) : Node(location)
+
+
+sealed class Declaration(
+        location: Location) : Node(location)
+
+data class LetDeclaration(
+        override val location: Location,
+        val name: ID,
+        val expression: Expression) : Declaration(location)
+
+
+data class ID(
+        override val location: Location,
+        val name: String) : Node(location)
+
+
+sealed class Expression(
+        location: Location) : Node(location)
+
+data class ConstantBool(
+        override val location: Location,
+        val value: Boolean) : Expression(location)
+
+data class ConstantInt(
+        override val location: Location,
+        val value: Int) : Expression(location)
+
+data class ConstantString(
+        override val location: Location,
+        val value: String) : Expression(location)
+
+data class IdReference(
+        override val location: Location,
+        val name: String) : Expression(location)
+
+data class IfExpression(
+        override val location: Location,
+        val guardExpression: Expression,
+        val thenExpression: Expression,
+        val elseExpression: Expression) : Expression(location)
+
+data class LambdaExpression(
+        override val location: Location,
+        val argument: ID,
+        val expression: Expression) : Expression(location)
+
+data class CallExpression(
+        override val location: Location,
+        val operator: Expression,
+        val operands: Expression) : Expression(location)
