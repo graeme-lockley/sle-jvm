0c2b28c95a553e95f8b80b23dcd7386a8bef7eb1
Graeme Lockley <graemel@no9.co.za>
2018-07-03 06:00:36 +0200

    chore: clean up j10-runtime

--- j10-runtime/src/main/java/za/co/no9/sle/runtime/Operators.java
+++ j10-runtime/src/main/java/za/co/no9/sle/runtime/Operators.java
@@ -1,4 +1,15 @@
 package za.co.no9.sle.runtime;
 
+import java.util.function.Function;
+
+
 public class Operators {
+    public static final Function<Integer, Function<Integer, Boolean>> EQUAL_EQUAL =
+            a -> (Function<Integer, Boolean>) a::equals;
+
+    public static final Function<Integer, Function<Integer, Integer>> STAR =
+            a -> (Function<Integer, Integer>) b -> a * b;
+
+    public static final Function<Integer, Function<Integer, Integer>> MINUS =
+            a -> (Function<Integer, Integer>) b -> a - b;
 }
