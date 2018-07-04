3f96406ea16be606e04c07176a6629fc3b48f9b0
Graeme Lockley <graemel@no9.co.za>
2018-07-04 06:11:51 +0200

    chore: update Firsty to import the runtime Builtin package

--- j10-translator/src/test/java/test/Firsty.java
+++ j10-translator/src/test/java/test/Firsty.java
@@ -2,6 +2,9 @@ package test;
 
 import java.util.function.Function;
 
+import static za.co.no9.sle.runtime.Builtin.*;
+
+
 public class Firsty {
     /**
      * factorial: Integer -> Integer
@@ -12,47 +15,6 @@ public class Firsty {
             return EQUAL_EQUAL.apply(n).apply(0) ? 1 : STAR.apply(n).apply(factorial.apply(MINUS.apply(n).apply(1)));
         }
     };
-
-
-    public static final Function<Integer, Function<Integer, Boolean>> EQUAL_EQUAL =
-            new Function<Integer, Function<Integer, Boolean>>() {
-                @Override
-                public Function<Integer, Boolean> apply(Integer a) {
-                    return new Function<Integer, Boolean>() {
-                        @Override
-                        public Boolean apply(Integer b) {
-                            return a.equals(b);
-                        }
-                    };
-                }
-            };
-
-
-    public static final Function<Integer, Function<Integer, Integer>> STAR =
-            new Function<Integer, Function<Integer, Integer>>() {
-                @Override
-                public Function<Integer, Integer> apply(Integer a) {
-                    return new Function<Integer, Integer>() {
-                        @Override
-                        public Integer apply(Integer b) {
-                            return a * b;
-                        }
-                    };
-                }
-            };
-
-    public static final Function<Integer, Function<Integer, Integer>> MINUS =
-            new Function<Integer, Function<Integer, Integer>>() {
-                @Override
-                public Function<Integer, Integer> apply(Integer a) {
-                    return new Function<Integer, Integer>() {
-                        @Override
-                        public Integer apply(Integer b) {
-                            return a - b;
-                        }
-                    };
-                }
-            };
 
 
     public static void main(String args[]) {
