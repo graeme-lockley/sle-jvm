424fec5914b4af34e43f5ae73396851094d5c7f8
Graeme Lockley <graemel@no9.co.za>
2018-07-06 06:43:43 +0200

    feature: add a maven-plugin module which will be used as the basis for building the Java SLE translator

---
+++ j8-maven-plugin/src/main/java/za/co/no9/sle/mojo/SLEMojo.java
@@ -0,0 +1,24 @@
+package za.co.no9.sle.mojo;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+
+@Mojo(name = "j8-sle")
+public class SLEMojo extends AbstractMojo {
+    @Parameter(defaultValue = "${project.basedir}/src/main/sle", alias = "sourceDirectory")
+    private String sourceDirectory;
+
+    @Parameter(defaultValue = "${project.basedir}/target/generated-sources/sle/java<", alias = "outputDirectory")
+    private String outputDirectory;
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        getLog().error("SLEMojo: sourceDirectory: " + sourceDirectory);
+        getLog().error("SLEMojo: outputDirectory: " + outputDirectory);
+    }
+}
+
