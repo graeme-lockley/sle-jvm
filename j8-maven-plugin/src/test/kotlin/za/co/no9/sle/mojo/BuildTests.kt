a0f6565e92fd1429b9fa6edd95006337a9900122
Graeme Lockley <graemel@no9.co.za>
2018-07-07 15:25:27 +0200

    test: an invalid source directory will report an error

--- j8-maven-plugin/src/test/kotlin/za/co/no9/sle/mojo/BuildTests.kt
+++ j8-maven-plugin/src/test/kotlin/za/co/no9/sle/mojo/BuildTests.kt
@@ -1,9 +1,90 @@
 package za.co.no9.sle.mojo
 
+import io.kotlintest.matchers.boolean.shouldBeTrue
 import io.kotlintest.specs.StringSpec
+import org.apache.maven.plugin.logging.Log
+import java.io.File
 
 class BuildTests : StringSpec({
-    "First test" {
+    val homeDir =
+            File(System.getProperty("user.dir") ?: "")
 
+    val testsResourcesDir =
+            File(homeDir, "src/test/resources/tests")
+
+
+    "An invalid source directory will report an error" {
+        val log =
+                TestLog()
+
+        build(log, File(testsResourcesDir, "unknown").absolutePath, ".")
+
+        log.items.any { it.contains(Regex("^E: Source.*is not a valid directory$")) }.shouldBeTrue()
     }
 })
+
+
+class TestLog : Log {
+    val items =
+            mutableListOf<String>()
+
+    override fun warn(item: CharSequence?) {
+        items.add("W: " + item.toString())
+    }
+
+    override fun warn(item: CharSequence?, p1: Throwable?) {
+        items.add("W: " + item.toString())
+    }
+
+    override fun warn(item: Throwable?) {
+        items.add("W: " + item.toString())
+    }
+
+    override fun info(item: CharSequence?) {
+        items.add("I: $item")
+    }
+
+    override fun info(item: CharSequence?, p1: Throwable?) {
+        items.add("I: $item")
+    }
+
+    override fun info(item: Throwable?) {
+        items.add("I: $item")
+    }
+
+    override fun isInfoEnabled(): Boolean =
+            true
+
+    override fun isErrorEnabled(): Boolean =
+            true
+
+    override fun isWarnEnabled(): Boolean =
+            true
+
+    override fun error(item: CharSequence?) {
+        items.add("E: $item")
+    }
+
+    override fun error(item: CharSequence?, p1: Throwable?) {
+        items.add("E: $item")
+    }
+
+    override fun error(item: Throwable?) {
+        items.add("E: $item")
+    }
+
+    override fun isDebugEnabled(): Boolean =
+            true
+
+    override fun debug(item: CharSequence?) {
+        items.add("D: $item")
+    }
+
+    override fun debug(item: CharSequence?, p1: Throwable?) {
+        items.add("D: $item")
+    }
+
+    override fun debug(item: Throwable?) {
+        items.add("D: $item")
+    }
+}
