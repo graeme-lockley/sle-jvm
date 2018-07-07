6271613e8effa88123f97290fe30b27fed9c9533
Graeme Lockley <graemel@no9.co.za>
2018-07-07 14:53:10 +0200

    chore: wire in kotlin tests into the module

--- j8-maven-plugin/src/main/kotlin/za/co/no9/sle/mojo/Build.kt
+++ j8-maven-plugin/src/main/kotlin/za/co/no9/sle/mojo/Build.kt
@@ -1,7 +1,23 @@
 package za.co.no9.sle.mojo
 
 import org.apache.maven.plugin.logging.Log
+import java.io.File
 
 fun build(log: Log, source: String, target: String) {
     log.info("Kotlin: $source  $target")
+
+    val sourceFile =
+            File(source)
+
+    val targetFile =
+            File(target)
+
+    if (!sourceFile.isDirectory) {
+        log.error("Source $source is not a valid directory")
+        return
+    }
+    if (!targetFile.isDirectory) {
+        log.error("Target $target is not a valid directory")
+        return
+    }
 }
---
+++ j8-maven-plugin/src/test/kotlin/za/co/no9/sle/mojo/BuildTests.kt
@@ -0,0 +1,9 @@
+package za.co.no9.sle.mojo
+
+import io.kotlintest.specs.StringSpec
+
+class BuildTests : StringSpec({
+    "First test" {
+
+    }
+})
