a03cdf5ba9470bc92fbab87fcdf1f93787dd3ef8
Graeme Lockley <graemel@no9.co.za>
2018-07-10 05:49:01 +0200

    test: simplified the module test to remove the mutually recursive requirement

--- j8-parser/src/test/kotlin/za/co/no9/sle/pass3/InferModuleTests.kt
+++ j8-parser/src/test/kotlin/za/co/no9/sle/pass3/InferModuleTests.kt
@@ -2,24 +2,38 @@ package za.co.no9.sle.pass3
 
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
-import za.co.no9.sle.*
-import za.co.no9.sle.typing.Environment
-import za.co.no9.sle.typing.Schema
-import za.co.no9.sle.typing.TArr
-import za.co.no9.sle.typing.typeInt
 import za.co.no9.sle.pass1.parseTreeToAST
 import za.co.no9.sle.pass2.astToCoreAST
+import za.co.no9.sle.right
+import za.co.no9.sle.typing.*
 
 
 class InferModuleTests : StringSpec({
-    "\"let add a b = a + b\nlet inc = add 1\"" {
+    "\"let add a b = a + b\"" {
         val environment =
                 Environment(mapOf(
                         Pair("(+)", Schema(listOf(), TArr(typeInt, TArr(typeInt, typeInt))))))
 
         val module =
-                astToCoreAST(parseTreeToAST(za.co.no9.sle.parser.parseModule("let add a b = a + b\n" +
-                        "let inc = add 1").right()!!.node))
+                astToCoreAST(parseTreeToAST(za.co.no9.sle.parser.parseModule("let add a b = a + b").right()!!.node))
+
+        val inferResult =
+                infer(module, environment).right()!!
+
+        inferResult.second.toString()
+                .shouldBe(
+                        "Int -> Int -> Int : '0 -> '2, " +
+                                "'2 : '1 -> '3")
+    }
+
+
+    "!\"let add a b : Int -> Int -> Int = something a b\"" {
+        val environment =
+                Environment(mapOf(
+                        Pair("something", Schema(listOf(1), TArr(TVar(1), TArr(TVar(1), TVar(1)))))))
+
+        val module =
+                astToCoreAST(parseTreeToAST(za.co.no9.sle.parser.parseModule("let add a b : Int -> Int -> Int = something a b").right()!!.node))
 
         val inferResult =
                 infer(module, environment).right()!!
