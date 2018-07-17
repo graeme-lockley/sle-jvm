a0a262f3c07a548b177d4428751619bb044408d1
Graeme Lockley <graemel@no9.co.za>
2018-07-17 05:52:49 +0200

    refactor: move test off of using a bespoke toString to dumping an object's contents

--- j8-parser/src/test/kotlin/za/co/no9/sle/parseTreeToASTTranslator/RunnerOfTests.kt
+++ j8-parser/src/test/kotlin/za/co/no9/sle/parseTreeToASTTranslator/RunnerOfTests.kt
@@ -5,10 +5,12 @@ import io.kotlintest.matchers.types.shouldBeTypeOf
 import io.kotlintest.shouldBe
 import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.FunSpec
+import org.antlr.v4.runtime.misc.Utils.spaces
 import za.co.no9.sle.Either
 import za.co.no9.sle.parser.Result
 import za.co.no9.sle.right
 import java.io.File
+import kotlin.reflect.full.memberProperties
 
 class RunnerOfTests : FunSpec({
     val testRoot =
@@ -32,12 +34,11 @@ class RunnerOfTests : FunSpec({
                 result.shouldBeTypeOf<Either.Value<Result>>()
 
                 val actual =
-                        toString(result.right()!!, 0)
+                        dumpString(result.right()!!)
                                 .split("\n")
                                 .map { it.trim() }
                                 .filter { it.isNotEmpty() }
                                 .joinToString("\n")
-
 
                 val expected =
                         astTest
@@ -48,7 +49,7 @@ class RunnerOfTests : FunSpec({
                 if (actual == expected) {
                     actual.shouldBe(expected)
                 } else {
-                    toString(result.right()!!, 0)
+                    dumpString(result.right()!!)
                             .shouldBe(astTest.joinToString("\n"))
                 }
             }
@@ -96,3 +97,49 @@ private fun readFile(file: File): Map<String, List<String>> {
 
     return result.toMap()
 }
+
+
+private fun dumpString(o: Any?, indent: Int = 0): String {
+    fun dd(label: String, value: Any?, indent: Int): String =
+            if (value == null) {
+                "${spaces(indent)}$label: null\n"
+            } else if (value is Int) {
+                "${spaces(indent)}$label: $value\n"
+            } else if (value is Boolean) {
+                "${spaces(indent)}$label: $value\n"
+            } else if (value is String) {
+                "${spaces(indent)}$label: $value\n"
+            } else if (value is List<*>) {
+                if (value.isEmpty()) {
+                    "${spaces(indent)}$label: []\n"
+                } else {
+                    "${spaces(indent)}$label: [\n" +
+                            value.joinToString("") { dumpString(it, indent + 2) } +
+                            "${spaces(indent)}]\n"
+                }
+            } else {
+                "${spaces(indent)}$label:\n" +
+                        dumpString(value, indent + 2)
+            }
+
+    if (o == null) {
+        return ""
+    } else {
+        return "${spaces(indent)}${o.javaClass.kotlin.simpleName}:\n" +
+                o.javaClass.kotlin.memberProperties.map {
+                    try {
+                        val value =
+                                it.call(o)
+
+                        if (it.name == "location") {
+                            ""
+                        } else {
+                            dd(it.name, value, indent + 2)
+                        }
+                    } catch (e: Exception) {
+                        ""
+                    }
+                }.filter { it.isNotEmpty() }.joinToString("")
+    }
+}
+
