657de116b29eced3bb074876ab23671c6b0adbb3
Graeme Lockley <graemel@no9.co.za>
2018-07-18 06:05:32 +0200

    refactor: rename test package pass2 to astToCoreAST

--- j8-parser/src/test/kotlin/za/co/no9/sle/astToCoreAST/TransformTests.kt
+++ j8-parser/src/test/kotlin/za/co/no9/sle/astToCoreAST/TransformTests.kt
@@ -1,12 +1,10 @@
-package za.co.no9.sle.pass2
+package za.co.no9.sle.astToCoreAST
 
 import io.kotlintest.properties.assertAll
 import io.kotlintest.shouldBe
 import io.kotlintest.specs.StringSpec
 import za.co.no9.sle.Location
 import za.co.no9.sle.Position
-import za.co.no9.sle.astToCoreAST.ConstantBool
-import za.co.no9.sle.astToCoreAST.astToCoreAST
 import za.co.no9.sle.parseTreeToASTTranslator.*
 import za.co.no9.sle.typing.Schema
 import za.co.no9.sle.typing.TArr
@@ -27,7 +25,7 @@ class TransformTests : StringSpec({
 
 
     "astToCoreAST ID" {
-        astToCoreAST(ID(arbLocation, "MyName"))
+        astToCoreAST(za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "MyName"))
                 .shouldBe(za.co.no9.sle.astToCoreAST.ID(arbLocation, "MyName"))
     }
 
@@ -46,7 +44,7 @@ class TransformTests : StringSpec({
 
     "astToCoreAST ConstantInt expression" {
         assertAll { n: Int ->
-            astToCoreAST(ConstantInt(arbLocation, n))
+            astToCoreAST(za.co.no9.sle.parseTreeToASTTranslator.ConstantInt(arbLocation, n))
                     .shouldBe(za.co.no9.sle.astToCoreAST.ConstantInt(arbLocation, n))
         }
     }
@@ -54,7 +52,7 @@ class TransformTests : StringSpec({
 
     "astToCoreAST ConstantString expression" {
         assertAll { s: String ->
-            astToCoreAST(ConstantString(arbLocation, s))
+            astToCoreAST(za.co.no9.sle.parseTreeToASTTranslator.ConstantString(arbLocation, s))
                     .shouldBe(za.co.no9.sle.astToCoreAST.ConstantString(arbLocation, s))
         }
     }
@@ -67,39 +65,39 @@ class TransformTests : StringSpec({
 
 
     "astToCoreAST IdReference expression" {
-        astToCoreAST(IdReference(arbLocation, "Hello"))
+        astToCoreAST(za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "Hello"))
                 .shouldBe(za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "Hello"))
     }
 
 
     "astToCoreAST IfExpression " {
-        astToCoreAST(IfExpression(arbLocation, IdReference(arbLocation, "a"), IdReference(arbLocation, "b"), IdReference(arbLocation, "c")))
+        astToCoreAST(za.co.no9.sle.parseTreeToASTTranslator.IfExpression(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "a"), za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "b"), za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "c")))
                 .shouldBe(za.co.no9.sle.astToCoreAST.IfExpression(arbLocation, za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "a"), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "b"), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "c")))
     }
 
 
     "astToCoreAST LambdaExpression" {
-        astToCoreAST(LambdaExpression(arbLocation, listOf(ID(arbLocation, "a"), ID(arbLocation, "b")), IdReference(arbLocation, "a")))
+        astToCoreAST(za.co.no9.sle.parseTreeToASTTranslator.LambdaExpression(arbLocation, listOf(za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "a"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "b")), za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "a")))
                 .shouldBe(za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "a"), za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "b"), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "a"))))
     }
 
 
     "astToCoreAST BinaryOpExpression" {
-        astToCoreAST(BinaryOpExpression(arbLocation, IdReference(arbLocation, "a"), ID(arbLocation, "+"), IdReference(arbLocation, "b")))
+        astToCoreAST(BinaryOpExpression(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "a"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "+"), za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "b")))
                 .shouldBe(za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "(+)"), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "a")), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "b")))
 
     }
 
     "astToCoreAST CallExpression" {
-        astToCoreAST(CallExpression(arbLocation, IdReference(arbLocation, "a"), listOf(IdReference(arbLocation, "b"), IdReference(arbLocation, "c"))))
+        astToCoreAST(za.co.no9.sle.parseTreeToASTTranslator.CallExpression(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "a"), listOf(za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "b"), za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "c"))))
                 .shouldBe(za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "a"), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "b")), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "c")))
     }
 
 
     "astToCoreAST module without type signature" {
         astToCoreAST(Module(arbLocation, listOf(
-                LetDeclaration(arbLocation, ID(arbLocation, "add"), listOf(ID(arbLocation, "a"), ID(arbLocation, "b")), null, BinaryOpExpression(arbLocation, IdReference(arbLocation, "a"), ID(arbLocation, "+"), IdReference(arbLocation, "b"))),
-                LetDeclaration(arbLocation, ID(arbLocation, "sub"), listOf(ID(arbLocation, "x"), ID(arbLocation, "y")), null, BinaryOpExpression(arbLocation, IdReference(arbLocation, "x"), ID(arbLocation, "-"), IdReference(arbLocation, "y"))))
+                LetDeclaration(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "add"), listOf(za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "a"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "b")), null, BinaryOpExpression(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "a"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "+"), za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "b"))),
+                LetDeclaration(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "sub"), listOf(za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "x"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "y")), null, BinaryOpExpression(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "x"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "-"), za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "y"))))
         )).shouldBe(za.co.no9.sle.astToCoreAST.Module(arbLocation, listOf(
                 za.co.no9.sle.astToCoreAST.LetDeclaration(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "add"), null, za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "a"), za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "b"), za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "(+)"), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "a")), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "b"))))),
                 za.co.no9.sle.astToCoreAST.LetDeclaration(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "sub"), null, za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "x"), za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "y"), za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "(-)"), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "x")), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "y")))))
@@ -109,8 +107,8 @@ class TransformTests : StringSpec({
 
     "astToCoreAST module with type signature" {
         astToCoreAST(Module(arbLocation, listOf(
-                LetDeclaration(arbLocation, ID(arbLocation, "add"), listOf(ID(arbLocation, "a"), ID(arbLocation, "b")), TArrow(arbLocation, TIdReference(arbLocation, "S"), TIdReference(arbLocation, "Int")), BinaryOpExpression(arbLocation, IdReference(arbLocation, "a"), ID(arbLocation, "+"), IdReference(arbLocation, "b"))),
-                LetDeclaration(arbLocation, ID(arbLocation, "sub"), listOf(ID(arbLocation, "x"), ID(arbLocation, "y")), TIdReference(arbLocation, "String"), BinaryOpExpression(arbLocation, IdReference(arbLocation, "x"), ID(arbLocation, "-"), IdReference(arbLocation, "y"))))
+                LetDeclaration(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "add"), listOf(za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "a"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "b")), TArrow(arbLocation, TIdReference(arbLocation, "S"), TIdReference(arbLocation, "Int")), BinaryOpExpression(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "a"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "+"), za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "b"))),
+                LetDeclaration(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "sub"), listOf(za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "x"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "y")), TIdReference(arbLocation, "String"), BinaryOpExpression(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "x"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "-"), za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "y"))))
         )).shouldBe(za.co.no9.sle.astToCoreAST.Module(arbLocation, listOf(
                 za.co.no9.sle.astToCoreAST.LetDeclaration(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "add"), Schema(emptyList(), TArr(TCon("S"), TCon("Int"))), za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "a"), za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "b"), za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "(+)"), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "a")), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "b"))))),
                 za.co.no9.sle.astToCoreAST.LetDeclaration(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "sub"), Schema(emptyList(), TCon("String")), za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "x"), za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "y"), za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "(-)"), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "x")), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "y")))))
@@ -120,8 +118,8 @@ class TransformTests : StringSpec({
 
     "astToCoreAST module with type alias and let declaration" {
         astToCoreAST(Module(arbLocation, listOf(
-                TypeAliasDeclaration(arbLocation, ID(arbLocation, "IntMap"), TArrow(arbLocation, TIdReference(arbLocation, "Int"), TIdReference(arbLocation, "Int"))),
-                LetDeclaration(arbLocation, ID(arbLocation, "sub"), listOf(ID(arbLocation, "x"), ID(arbLocation, "y")), TIdReference(arbLocation, "String"), BinaryOpExpression(arbLocation, IdReference(arbLocation, "x"), ID(arbLocation, "-"), IdReference(arbLocation, "y"))))
+                TypeAliasDeclaration(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "IntMap"), TArrow(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.TIdReference(arbLocation, "Int"), za.co.no9.sle.parseTreeToASTTranslator.TIdReference(arbLocation, "Int"))),
+                LetDeclaration(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "sub"), listOf(za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "x"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "y")), TIdReference(arbLocation, "String"), BinaryOpExpression(arbLocation, za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "x"), za.co.no9.sle.parseTreeToASTTranslator.ID(arbLocation, "-"), za.co.no9.sle.parseTreeToASTTranslator.IdReference(arbLocation, "y"))))
         )).shouldBe(za.co.no9.sle.astToCoreAST.Module(arbLocation, listOf(
                 za.co.no9.sle.astToCoreAST.TypeAliasDeclaration(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "IntMap"), Schema(listOf(), TArr(typeInt, typeInt))),
                 za.co.no9.sle.astToCoreAST.LetDeclaration(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "sub"), Schema(emptyList(), TCon("String")), za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "x"), za.co.no9.sle.astToCoreAST.LambdaExpression(arbLocation, za.co.no9.sle.astToCoreAST.ID(arbLocation, "y"), za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.CallExpression(arbLocation, za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "(-)"), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "x")), za.co.no9.sle.astToCoreAST.IdReference(arbLocation, "y")))))
