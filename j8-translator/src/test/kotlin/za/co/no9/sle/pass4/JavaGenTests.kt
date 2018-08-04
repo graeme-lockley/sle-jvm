7c326dacd03d70c464ba5d908ce43b3b327258cf
Graeme Lockley <graemel@no9.co.za>
2018-08-04 21:59:05 +0200

    refactor: pull the common code together

--- j8-translator/src/test/kotlin/za/co/no9/sle/pass4/JavaGenTests.kt
+++ j8-translator/src/test/kotlin/za/co/no9/sle/pass4/JavaGenTests.kt
@@ -9,58 +9,37 @@ import za.co.no9.sle.typing.*
 
 
 class JavaGenTests : StringSpec({
-    "Compile test/First.sle" {
+    fun stuff(name: String, environment: Environment) {
         val input =
-                this.javaClass.getResource("/test/First.sle").readText()
-
-        val environment =
-                Environment(mapOf(
-                        Pair("(==)", Schema(listOf(), TArr(typeInt, TArr(typeInt, typeBool)))),
-                        Pair("(-)", Schema(listOf(), TArr(typeInt, TArr(typeInt, typeInt)))),
-                        Pair("(*)", Schema(listOf(), TArr(typeInt, TArr(typeInt, typeInt))))))
+                this.javaClass.getResource("/test/$name.sle").readText()
 
         val result =
                 parseWithDetail(input, environment)
-                        .map { translateToJava(it.resolvedModule, "test", "First") }
+                        .map { translateToJava(it.resolvedModule, "test", name) }
                         .map { it.toString() }
 
-        this.javaClass.getResource("/test/First.java").readText()
+        this.javaClass.getResource("/test/$name.java").readText()
                 .shouldBe(result.right())
+
     }
+
+
+    "Compile test/First.sle" {
+        stuff("First", Environment(mapOf(
+                Pair("(==)", Schema(listOf(), TArr(typeInt, TArr(typeInt, typeBool)))),
+                Pair("(-)", Schema(listOf(), TArr(typeInt, TArr(typeInt, typeInt)))),
+                Pair("(*)", Schema(listOf(), TArr(typeInt, TArr(typeInt, typeInt)))))))
+    }
+
 
     "Compile test/TypeReference.sle" {
-        val input =
-                this.javaClass.getResource("/test/TypeReference.sle").readText()
-
-        val environment =
-                Environment(mapOf(
-                        Pair("(==)", Schema(listOf(Parameter(1, null)), TArr(TVar(1), TArr(TVar(1), typeBool))))
-                ))
-
-        val result =
-                parseWithDetail(input, environment)
-                        .map { translateToJava(it.resolvedModule, "test", "TypeReference") }
-                        .map { it.toString() }
-
-        this.javaClass.getResource("/test/TypeReference.java").readText()
-                .shouldBe(result.right())
+        stuff("TypeReference", Environment(mapOf(
+                Pair("(==)", Schema(listOf(Parameter(1, null)), TArr(TVar(1), TArr(TVar(1), typeBool))))
+        )))
     }
 
+
     "Compile test/UnitValue.sle" {
-        val input =
-                this.javaClass.getResource("/test/UnitValue.sle").readText()
-
-        val environment =
-                Environment(mapOf(
-                        Pair("(==)", Schema(listOf(Parameter(1, null)), TArr(TVar(1), TArr(TVar(1), typeBool))))
-                ))
-
-        val result =
-                parseWithDetail(input, environment)
-                        .map { translateToJava(it.resolvedModule, "test", "UnitValue") }
-                        .map { it.toString() }
-
-        this.javaClass.getResource("/test/UnitValue.java").readText()
-                .shouldBe(result.right())
+        stuff("UnitValue", emptyEnvironment)
     }
 })
