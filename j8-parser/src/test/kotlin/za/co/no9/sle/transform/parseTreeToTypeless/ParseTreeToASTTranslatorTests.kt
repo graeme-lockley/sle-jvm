9cf1ce5babd2a5f174780c7ac45fdd451e55f7df
Graeme Lockley <graemel@no9.co.za>
2018-09-15 12:24:25 +0200

    refactor: move the parseTreeToCore tests

--- j8-parser/src/test/kotlin/za/co/no9/sle/transform/parseTreeToTypeless/ParseTreeToASTTranslatorTests.kt
+++ j8-parser/src/test/kotlin/za/co/no9/sle/transform/parseTreeToTypeless/ParseTreeToASTTranslatorTests.kt
@@ -11,7 +11,7 @@ import java.util.function.Consumer
 
 
 class ParseTreeToASTTranslatorTests : FunSpec({
-    runner(this, "parseTreeToASTTranslator", RunnerConsumer())
+    runner(this, "parseTreeToTypeless", RunnerConsumer())
 })
 
 
