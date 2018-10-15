46c70e860ce2da3a74ac35a9c1cd7986f96240a6
Graeme Lockley <graemel@no9.co.za>
2018-10-15 05:39:00 +0200

    feature: create a new pass from enriched core to core

---
+++ j8-parser/src/main/kotlin/za/co/no9/sle/transform/enrichedCoreToCore/Transform.kt
@@ -0,0 +1,86 @@
+package za.co.no9.sle.transform.enrichedCoreToCore
+
+import za.co.no9.sle.Either
+import za.co.no9.sle.Errors
+import za.co.no9.sle.ast.enrichedCore.*
+import za.co.no9.sle.ast.enrichedCore.Unit
+import za.co.no9.sle.map
+import za.co.no9.sle.transform.typelessPatternToTypedPattern.Constraints
+import za.co.no9.sle.typing.Environment
+import za.co.no9.sle.typing.Substitution
+
+
+data class Detail(
+        val constraints: Constraints,
+        val substitution: Substitution,
+        val unresolvedModule: za.co.no9.sle.ast.typedPattern.Module,
+        val resolvedModule: za.co.no9.sle.ast.typedPattern.Module,
+        val enrichedCore: za.co.no9.sle.ast.enrichedCore.Module,
+        val coreModule: Module)
+
+
+fun parseWithDetail(text: String, environment: Environment): Either<Errors, Detail> {
+    val typePatternDetail =
+            za.co.no9.sle.transform.typedPatternToEnrichedCore.parseWithDetail(text, environment)
+
+    return typePatternDetail.map {
+        Detail(it.constraints, it.substitution, it.unresolvedModule, it.resolvedModule, it.enrichedModule, transform(it.enrichedModule))
+    }
+}
+
+
+fun parse(text: String, environment: Environment): Either<Errors, Module> {
+    return za.co.no9.sle.transform.typedPatternToEnrichedCore.parse(text, environment).map { transform(it) }
+}
+
+
+private fun transform(module: za.co.no9.sle.ast.enrichedCore.Module): Module =
+        Module(module.location, module.declarations.map { transform(it) })
+
+
+private fun transform(declaration: za.co.no9.sle.ast.enrichedCore.Declaration): Declaration =
+        when (declaration) {
+            is za.co.no9.sle.ast.enrichedCore.LetDeclaration ->
+                LetDeclaration(declaration.location, declaration.scheme, transform(declaration.name), transform(declaration.expression))
+
+            is za.co.no9.sle.ast.enrichedCore.TypeAliasDeclaration ->
+                TypeAliasDeclaration(declaration.location, transform(declaration.name), declaration.scheme)
+
+            is za.co.no9.sle.ast.enrichedCore.TypeDeclaration ->
+                TypeDeclaration(declaration.location, transform(declaration.name), declaration.scheme, declaration.constructors.map { transform(it) })
+        }
+
+private fun transform(expression: za.co.no9.sle.ast.enrichedCore.Expression): Expression =
+        when (expression) {
+            is za.co.no9.sle.ast.enrichedCore.Unit ->
+                Unit(expression.location, expression.type)
+
+            is za.co.no9.sle.ast.enrichedCore.ConstantBool ->
+                ConstantBool(expression.location, expression.type, expression.value)
+
+            is za.co.no9.sle.ast.enrichedCore.ConstantInt ->
+                ConstantInt(expression.location, expression.type, expression.value)
+
+            is za.co.no9.sle.ast.enrichedCore.ConstantString ->
+                ConstantString(expression.location, expression.type, expression.value)
+
+            is za.co.no9.sle.ast.enrichedCore.IdReference ->
+                IdReference(expression.location, expression.type, expression.name)
+
+            is za.co.no9.sle.ast.enrichedCore.IfExpression ->
+                IfExpression(expression.location, expression.type, transform(expression.guardExpression), transform(expression.thenExpression), transform(expression.elseExpression))
+
+            is za.co.no9.sle.ast.enrichedCore.LambdaExpression ->
+                LambdaExpression(expression.location, expression.type, transform(expression.argument), transform(expression.expression))
+
+            is za.co.no9.sle.ast.enrichedCore.CallExpression ->
+                CallExpression(expression.location, expression.type, transform(expression.operator), transform(expression.operand))
+        }
+
+
+private fun transform(name: za.co.no9.sle.ast.enrichedCore.ID): ID =
+        ID(name.location, name.name)
+
+
+private fun transform(constructor: za.co.no9.sle.ast.enrichedCore.Constructor): Constructor =
+        Constructor(constructor.location, transform(constructor.name), constructor.arguments)
---
+++ j8-parser/src/test/kotlin/za/co/no9/sle/transform/enrichedCoreToCore/TypedPatternToTypedCoreTests.kt
@@ -0,0 +1,87 @@
+package za.co.no9.sle.transform.enrichedCoreToCore
+
+import io.kotlintest.matchers.types.shouldBeTypeOf
+import io.kotlintest.specs.FunSpec
+import za.co.no9.sle.*
+import za.co.no9.sle.typing.*
+import java.util.function.Consumer
+
+
+class EnrichedCoreToCoreTests : FunSpec({
+    runner(this, "enrichedCoreToCore", RunnerConsumer())
+})
+
+
+private class RunnerConsumer : Consumer<Map<String, List<String>>> {
+    private val environment =
+            initialEnvironment
+                    .newValue("(+)", Scheme(listOf(), TArr(typeInt, TArr(typeInt, typeInt))))
+                    .newValue("(-)", Scheme(listOf(), TArr(typeInt, TArr(typeInt, typeInt))))
+                    .newValue("(*)", Scheme(listOf(), TArr(typeInt, TArr(typeInt, typeInt))))
+                    .newValue("(/)", Scheme(listOf(), TArr(typeInt, TArr(typeInt, typeInt))))
+                    .newValue("(&&)", Scheme(listOf(), TArr(typeBool, TArr(typeBool, typeBool))))
+                    .newValue("(==)", Scheme(listOf(1), TArr(TVar(homeLocation, 1), TArr(TVar(homeLocation, 1), typeBool))))
+                    .newValue("aString", Scheme(listOf(), typeString))
+
+
+    override fun accept(fileContent: Map<String, List<String>>) {
+        val parseWithDetail =
+                parseWithDetail(fileContent["src"]?.joinToString("\n") ?: "", environment)
+
+
+        val constraints =
+                fileContent["constraints"]
+
+        if (constraints != null) {
+            parseWithDetail.shouldBeTypeOf<Either.Value<Detail>>()
+            parseWithDetail.right()!!.constraints.state.map { it.toString() }.shouldBeEqual(constraints)
+        }
+
+
+        val expectedSubstitution =
+                fileContent["substitution"]
+
+        if (expectedSubstitution != null) {
+            parseWithDetail.shouldBeTypeOf<Either.Value<Detail>>()
+            parseWithDetail.right()!!.substitution.state.map { it.toString() }.shouldBeEqual(expectedSubstitution)
+        }
+
+
+        val astTest =
+                fileContent["ast"]
+
+        if (astTest != null) {
+            parseWithDetail.shouldBeTypeOf<Either.Value<Detail>>()
+            parseWithDetail.right()!!.unresolvedModule.shouldBeEqual(astTest)
+        }
+
+
+        val typeAST =
+                fileContent["typeAST"]
+
+        if (typeAST != null) {
+            parseWithDetail.shouldBeTypeOf<Either.Value<Detail>>()
+            parseWithDetail.right()!!.resolvedModule.shouldBeEqual(typeAST)
+        }
+
+
+        val coreAST =
+                fileContent["coreAST"]
+
+        if (coreAST != null) {
+            parseWithDetail.shouldBeTypeOf<Either.Value<Detail>>()
+            parseWithDetail.right()!!.coreModule.shouldBeEqual(coreAST)
+        }
+
+
+        val errors =
+                fileContent["errors"]
+
+        if (errors != null) {
+            parseWithDetail.shouldBeTypeOf<Either.Error<Detail>>()
+            parseWithDetail.left()!!.map { it.toString() }.shouldBeEqual(errors)
+        }
+    }
+}
+
+
