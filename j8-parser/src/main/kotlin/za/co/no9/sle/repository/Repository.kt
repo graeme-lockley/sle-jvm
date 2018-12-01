982e7abcf08c65532069842ace411aa59316b0c6
Graeme Lockley <graemel@no9.co.za>
2018-12-01 16:06:02 +0200

    refactor: remove unused import

--- j8-parser/src/main/kotlin/za/co/no9/sle/repository/Repository.kt
+++ j8-parser/src/main/kotlin/za/co/no9/sle/repository/Repository.kt
@@ -2,12 +2,11 @@ package za.co.no9.sle.repository
 
 import za.co.no9.sle.Either
 import za.co.no9.sle.Errors
-import za.co.no9.sle.QString
 import za.co.no9.sle.Source
 import java.io.File
 
 
-interface Repository<out T: Item> {
+interface Repository<out T : Item> {
     fun import(name: String): Either<Errors, Export>
 
     fun item(source: Source, inputFile: File): T
