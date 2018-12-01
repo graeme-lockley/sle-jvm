25c95ca7c97ddb8a273a369cb7cda78cccddacc6
Graeme Lockley <graemel@no9.co.za>
2018-12-01 10:52:21 +0200

    refactor: remove unused method

--- j8-parser/src/main/kotlin/za/co/no9/sle/typing/Type.kt
+++ j8-parser/src/main/kotlin/za/co/no9/sle/typing/Type.kt
@@ -8,6 +8,7 @@ import za.co.no9.sle.homeLocation
 sealed class Type(open val location: Location) {
     abstract fun apply(s: Substitution): Type
 
+
     abstract fun ftv(): Set<Var>
 }
 
@@ -19,8 +20,10 @@ data class TVar(
     override fun apply(s: Substitution) =
             s[variable] ?: this
 
+
     override fun ftv() =
             setOf(variable)
+
 
     override fun toString(): String =
             "'$variable"
@@ -31,7 +34,6 @@ data class TAlias(
         override val location: Location,
         val name: QString,
         val arguments: List<Type> = emptyList()) : Type(location) {
-    constructor(location: Location, name: String, arguments: List<Type> = emptyList()) : this(location, QString(name), arguments)
 
     override fun apply(s: Substitution) =
             if (arguments.isEmpty())
@@ -39,8 +41,10 @@ data class TAlias(
             else
                 TCon(location, name, arguments.map { it.apply(s) })
 
+
     override fun ftv() =
             arguments.fold(emptySet<Var>()) { ftvs, type -> ftvs + type.ftv() }
+
 
     override fun toString(): String =
             if (arguments.isEmpty())
@@ -54,7 +58,9 @@ data class TCon(
         override val location: Location,
         val name: QString,
         val arguments: List<Type> = emptyList()) : Type(location) {
+
     constructor(location: Location, name: String, arguments: List<Type> = emptyList()) : this(location, QString(name), arguments)
+
 
     override fun apply(s: Substitution) =
             if (arguments.isEmpty())
@@ -62,8 +68,10 @@ data class TCon(
             else
                 TCon(location, name, arguments.map { it.apply(s) })
 
+
     override fun ftv() =
             arguments.fold(emptySet<Var>()) { ftvs, type -> ftvs + type.ftv() }
+
 
     override fun toString(): String =
             if (arguments.isEmpty())
@@ -80,11 +88,14 @@ data class TArr(
 
     constructor(domain: Type, range: Type) : this(domain.location + range.location, domain, range)
 
+
     override fun apply(s: Substitution) =
             TArr(location, domain.apply(s), range.apply(s))
 
+
     override fun ftv() =
             domain.ftv().plus(range.ftv())
+
 
     override fun toString(): String =
             when (domain) {
