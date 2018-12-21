4b5dc2a934ff28ce784d20b5de11b3d5041e7a99
Graeme Lockley <graemel@no9.co.za>
2018-12-21 14:47:07 +0200

    feature: add builtin support for String.reverse

--- j8-runtime/src/main/java/za/co/no9/sle/runtime/StringUtil.java
+++ j8-runtime/src/main/java/za/co/no9/sle/runtime/StringUtil.java
@@ -5,4 +5,24 @@ import java.util.function.Function;
 public class StringUtil {
     public static final Object length =
             (Function<Object, Object>) s -> ((String) s).length();
+
+    public static final Object reverse =
+            (Function<Object, Object>) s -> {
+                String input =
+                        (String) s;
+
+                int upper =
+                        input.length();
+
+                StringBuilder result =
+                        new StringBuilder(upper);
+
+                int lp = upper - 1;
+                while (lp >= 0) {
+                    result.append(input.charAt(lp));
+                    lp -= 1;
+                }
+
+                return result.toString();
+            };
 }
