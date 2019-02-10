0e5e0719bd1d6cc7982ea876eb684199f769dfd1
Graeme Lockley <graemel@no9.co.za>
2019-02-10 11:22:18 +0200

    fix: assemble the output into a single expression to avoid threading interleaving the output

--- j8-runtime/src/main/java/za/co/no9/sle/runtime/DEBUG.java
+++ j8-runtime/src/main/java/za/co/no9/sle/runtime/DEBUG.java
@@ -5,10 +5,7 @@ import java.util.function.Function;
 public class DEBUG {
     public static final Object info =
             (Function<Object, Object>) message -> (Function<Object, Object>) value -> {
-                System.out.print("info: ");
-                System.out.print(message);
-                System.out.print(": ");
-                System.out.println(valueToString(value));
+                System.out.println("info: " + message + ": " + valueToString(value));
 
                 return value;
             };
@@ -16,10 +13,7 @@ public class DEBUG {
 
     public static final Object error =
             (Function<Object, Object>) message -> (Function<Object, Object>) value -> {
-                System.err.print("error: ");
-                System.err.print(message);
-                System.err.print(": ");
-                System.err.println(valueToString(value));
+                System.err.println("info: " + message + ": " + valueToString(value));
 
                 return value;
             };
