e6de140c7f0c18884997d600d600aeaa6f26a31d
Graeme Lockley <graemel@no9.co.za>
2019-02-16 10:35:45 +0200

    feature: https://trello.com/c/zVnTvVAD: add equals to Cmd

--- j8-actors/src/main/java/za/co/no9/sle/actors/Cmd.java
+++ j8-actors/src/main/java/za/co/no9/sle/actors/Cmd.java
@@ -25,4 +25,46 @@ public class Cmd<S, M> {
                 ", message=" + message +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        } else if (o instanceof Cmd){
+            Cmd cmdO = (Cmd) o;
+
+            return actorRef.equals(cmdO.actorRef) && equals(message, cmdO.message);
+        } else {
+            return false;
+        }
+    }
+
+
+    private static boolean equals(Object a, Object b) {
+        if (a == b) {
+            return true;
+        } else if (a instanceof Object[] && b instanceof Object[]) {
+            Object[] aArray =
+                    (Object[]) a;
+
+            Object[] bArray =
+                    (Object[]) b;
+
+            int aLength =
+                    aArray.length;
+
+            if (aLength == bArray.length) {
+                for (int lp = 0; lp < aLength; lp += 1) {
+                    if (!equals(aArray[lp], bArray[lp])) {
+                        return false;
+                    }
+                }
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return a.equals(b);
+        }
+    }
 }
