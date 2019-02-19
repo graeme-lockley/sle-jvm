130b04803db59d3135cc80e5cb5368fb8bd57c02
Graeme Lockley <graemel@no9.co.za>
2019-02-19 05:45:46 +0200

    feature: add a short actor wait duration after the tests have run to allow the actors under test to complete their activity

--- j8-sle-libs/src/test/kotlin/samples/SLETests.kt
+++ j8-sle-libs/src/test/kotlin/samples/SLETests.kt
@@ -2,6 +2,7 @@ package samples
 
 import io.kotlintest.specs.AbstractFunSpec
 import io.kotlintest.specs.FunSpec
+import za.co.no9.sle.runtime.ActorUtil
 import za.co.no9.sle.tools.test.Repository
 import za.co.no9.sle.tools.test.runTests
 import java.io.File
@@ -9,6 +10,7 @@ import java.io.File
 
 class SLETests : FunSpec({
     runner(this)
+    (ActorUtil.synchronousWait as java.util.function.Function<Int, Any>).apply(10)
 })
 
 
